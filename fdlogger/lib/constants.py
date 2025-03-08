d0f6e493a39a9debd76299e5097cc5704b001561
Justin Porteous <justin@porteous.co.za>
2025-03-08 16:08:16 +0200

    chore: Revert Constants

---
+++ fdlogger/lib/constants.py
@@ -0,0 +1,28 @@
+class CMD():
+    def GROUPQUERY():
+        return "GROUPQUERY"
+    
+    def PING():
+        return "PING"
+    
+    def RESPONSE():
+        return "RESPONSE"
+    
+    def CHAT():
+        return "CHAT"
+    
+    def CMDQUERY():
+        return "CMDQUERY"
+    
+    def POST():
+        return "POST"
+    
+    def LOG():
+        return "LOG"
+    
+    def UPDATE():
+        return "UPDATE"
+    
+    def CONFLICT():
+        return "CONFLICT"
+    
